28a492135d04590e915ce6199fb2fe201bb2d804
skylurJEON <skylur20@gmail.com>
2024-08-28 14:02:19 +0900

    change name, mp4 codec

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from speed_and_distance_estimator import SpeedAndDistance_Estimator
 
 def main():
     # Read Video
-    video_frames = read_video('input_videos/08fd33_4.mp4')
+    video_frames = read_video('input_videos/sample.mp4')
 
     # Initialize Tracker
     tracker = Tracker('models/best.pt')
@@ -81,7 +81,7 @@ def main():
     speed_and_distance_estimator.draw_speed_and_distance(output_video_frames,tracks)
 
     # Save video
-    save_video(output_video_frames, 'output_videos/output_video.avi')
+    save_video(output_video_frames, 'output_videos/output_video.mp4')
 
 if __name__ == '__main__':
     main()
